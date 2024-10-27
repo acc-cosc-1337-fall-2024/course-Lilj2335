8203a68894cd9b874f286044067401f2d19488aa
Lilj2335 <jeffrey.rhodan@g.austincc.edu>
2024-10-27 23:08:50 +0000

    msg

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,33 @@
 //add include statements
-
+#include "func.h"
 //add function code here
+double get_gc_content(const string& dna)
+{
+    double sum = 0;
+    for (auto i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] == 'G' || dna[i] == 'C') sum++;
+    }
+    return sum/dna.length();
+}
+string get_dna_complement(string dna)
+{
+    dna = reverse_string(dna);
+    for (auto i = 0; i < dna.length(); i++)
+    {
+        if(dna[i] == 'C') dna[i] = 'G';
+        else if(dna[i] == 'G') dna[i] = 'C';
+        else if(dna[i] == 'A') dna[i] = 'T';
+        else if(dna[i] == 'T') dna[i] = 'A';
+    }
+    return dna;
+}
+string reverse_string(string dna)
+{
+    string reversed = "";
+    for (int i = dna.length(); i >= 0; i--)
+    {
+        reversed += dna[i];
+    }
+    return reversed;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,6 +1,25 @@
+#include <iostream>
+#include "func.h"
 
-int main() 
+using namespace std;
+int main()
 {
-
-	return 0;
+	int menu_input;
+	cout<<"1. Get GC Content \n" << "2. Get DNA Complement \n" << "3. Exit \n";
+	cin>>menu_input;
+	if(menu_input == 1)
+	{
+		string dna;
+		cout<<"input string: ";
+		cin>>dna;
+		cout<<"GC Content: "<<get_gc_content(dna)<<"\n";
+	}
+	if(menu_input == 2)
+	{
+		string dna;
+		cout<<"input string: ";
+		cin>>dna;
+		cout<<"DNA Complement: "<<get_dna_complement(dna)<<"\n";
+	}
 }
+
